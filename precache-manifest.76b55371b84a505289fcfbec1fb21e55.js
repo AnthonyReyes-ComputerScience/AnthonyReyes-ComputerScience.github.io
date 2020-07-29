ff141312416dbc01a03f53e348b935fb3ca163c1
AnthonyReyes-ComputerScience <reyes.anthony1437@gmail.com>
2020-07-29 05:40:49 -0400

    First Build

---
+++ precache-manifest.76b55371b84a505289fcfbec1fb21e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df5d8031de7241040dd8ad75faaa325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5183bc01a26474c280d",
+    "url": "/static/css/main.096bf5e7.chunk.css"
+  },
+  {
+    "revision": "a96e43ce06bf72e6d7cb",
+    "url": "/static/js/2.f1c4a147.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/static/js/2.f1c4a147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5183bc01a26474c280d",
+    "url": "/static/js/main.ab8cc415.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
